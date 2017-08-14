1c1f31c2be24a54bd3d3a5d3bc9b33cf91583f1e
Santhoshkumar <santhp@thoughtworks.com>
2017-08-14 12:41:46 +0530

    <Santhosh/Deepak> Initial working project setup

---
+++ src/main/java/com/tw/tradeaway/StarterMain.java
@@ -0,0 +1,15 @@
+package com.tw.tradeaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterMain {
+
+    public static void main(final String... args) {
+
+        System.out.println("Application started.");
+        SpringApplication.run(StarterMain.class, args);
+    }
+
+}
