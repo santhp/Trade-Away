088c5c46f189f0a7f2bfe428935233fc90c841ea
Santhoshkumar <santhp@thoughtworks.com>
2017-08-16 15:28:47 +0530

    <Pavan/Santhosh> Integrated category list with backend[2]

--- src/main/frontend/app/components/item.js
+++ src/main/frontend/app/components/item.js
@@ -7,7 +7,23 @@ class Item extends Component {
 
     render() {
         return <div>
-
+            <table style={{width: '100%'}}>
+                <tbody>
+                <tr>
+                    <td rowSpan="2">
+                        <img src={this.props.details.imageUrl} style={{height: 100, width: 100}} alt='product'/>
+                    </td>
+                    <td>{this.props.details.name}</td>
+                    <td>Rs. {this.props.details.price}</td>
+                    <td style={{'textAlign': 'right'}}>
+                        <button>Buy</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td colSpan="3">{this.props.details.description}</td>
+                </tr>
+                </tbody>
+            </table>
         </div>;
     }
 }
