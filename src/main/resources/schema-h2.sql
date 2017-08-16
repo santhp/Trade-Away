ac90356877a92b54db3936c72656fb9c8a29a861
ksandeep <ksandeep@in-blr-ksandeep.local>
2017-08-16 17:33:38 +0530

    merged conflicts

--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -4,9 +4,5 @@ id INTEGER NOT NULL,
 name VARCHAR(100) NOT NULL,
 email VARCHAR(100) DEFAULT NULL,
 address VARCHAR(100) DEFAULT NULL,
-<<<<<<< HEAD
-PRIMARY KEY (id));
-
-=======
 PRIMARY KEY (id));*/
->>>>>>> Order creation success case verified
+
