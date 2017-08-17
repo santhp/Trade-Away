d27aa83ad41b08ebb35da301281794c021835b99
ksandeep <ksandeep@in-blr-ksandeep.local>
2017-08-17 16:07:00 +0530

    Added new component for Order Confirmation. #Pavan/Sandeep

---
+++ src/main/frontend/app/components/order-confirmation.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {browserHistory} from 'react-router';
+
+class OrderConfirmation extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+
+            <div>
+            <h2> Your Order is successfully placed and the seller would process the order shortly </h2>
+
+        </div>
+
+    );
+    }
+}
+
+export default OrderConfirmation;
